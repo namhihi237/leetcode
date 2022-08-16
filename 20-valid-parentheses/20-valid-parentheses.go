3cb2d73eb6c1f78a8b6b11c6b4c419c50f24a9ff
NamLe <39086067+namhihi237@users.noreply.github.com>
2022-08-16 14:09:43 +0700

    Time: 0 ms (100.00%), Space: 2.2 MB (15.55%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.go
+++ 20-valid-parentheses/20-valid-parentheses.go
@@ -1,6 +1,5 @@
 func isValid(s string) bool {
 	stack := []string{}
-	open := []string{"(", "{", "["}
 	object := map[string]string{
 		")": "(",
 		"}": "{",
@@ -8,28 +7,19 @@ func isValid(s string) bool {
 	}
 
 	for i := 0; i < len(s); i++ {
-		if isOpen(string(s[i]), open) {
+		switch string(s[i]) {
+		case "(", "[", "{":
 			stack = append(stack, string(s[i]))
-		} else {
+		default:
 			if len(stack) == 0 {
 				return false
-			}
-
-			if stack[len(stack)-1] == object[string(s[i])] {
+			} else if stack[len(stack)-1] == object[string(s[i])] {
 				stack = stack[:len(stack)-1]
 			} else {
 				return false
 			}
+
 		}
 	}
 	return len(stack) == 0
-}
-
-func isOpen(item string, open []string) bool {
-	for i := 0; i < len(open); i++ {
-		if item == open[i] {
-			return true
-		}
-	}
-	return false
 }

750fe091511041a89cd15e911a0026279e85d0c3
NamLe <39086067+namhihi237@users.noreply.github.com>
2022-09-09 10:10:11 +0700

    Time: 103 ms (80.89%), Space: 53.7 MB (10.28%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.js
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.js
@@ -1,27 +1,29 @@
-/**
- * @param {string} s
- * @return {string}
- */
 var reverseVowels = function (s = "") {
-  const vowels = [];
-  let result = "";
-  for (let i = 0; i < s.length; i++) {
-    if (checkVowels(s[i])) {
-      vowels.push(s[i]);
+  let result = [];
+  let i = 0, j = s.length - 1;
+
+  while (i <= j) {
+    if (!checkVowels(s[i])) {
+      result[i] = s[i];
+      i++;
+      continue;
     }
+
+    if (!checkVowels(s[j])) {
+      result[j] = s[j];
+      j--;
+      continue;
+    }
+
+    result[i] = s[j];
+    result[j] = s[i];
+    i++;
+    j--;
   }
 
-  for (let i = 0; i < s.length; i++) {
-    if (checkVowels(s[i])) {
-      result += vowels.pop();
-    } else {
-      result += s[i];
-    }
-  }
-
-  return result;
+  return result.join('');
 };
 
 var checkVowels = (c) => {
-  return ['a', 'e', 'i', 'o', 'u'].includes(c.toLowerCase())
+  return ['a', 'e', 'i', 'o', 'u'].includes(c?.toLowerCase())
 }

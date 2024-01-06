2952846f5f9d5f12dbb4758e987cd596f3f0f893
Nam Le <39086067+namhihi237@users.noreply.github.com>
2024-01-06 22:47:50 +0700

    Time: 74 ms (33.52%), Space: 45.1 MB (17.10%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -1,12 +1,11 @@
 var missingNumber = function(nums) {
     const sorted = nums.sort((a,b) => a - b);
-    let n = -1;
+    if (sorted[0] !== 0) return 0;
 
-    for (let i = 0; i < sorted.length; i++) {
-      if (n + 1 !== sorted[i]) {
-        return n+1;
+    for (let i = 0; i < sorted.length -1; i++) {
+      if (sorted[i] + 1 !== sorted[i + 1]) {
+        return sorted[i] + 1;
       }
-      n++;
     }
-    return n + 1;
+    return sorted[sorted.length -1] + 1;
 };

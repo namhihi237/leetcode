fadfc546231204114da82b133cac7901068c243a
Nam Le <39086067+namhihi237@users.noreply.github.com>
2024-08-12 21:06:15 +0700

    Time: 33 ms (99.89%) | Memory: 49.5 MB (9.56%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  let j = 0;
+  for (let i = m; i < m + n; i++) {
+    nums1[i] = nums2[j];
+    j++;
+  }
+  nums1 = nums1.sort((a,b) => a - b);
+};

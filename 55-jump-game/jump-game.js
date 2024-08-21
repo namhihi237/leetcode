2c422bde0237bb988f61581cb758f192fa2bf18d
Nam Le <39086067+namhihi237@users.noreply.github.com>
2024-08-21 21:59:17 +0700

    Time: 65 ms (55.55%) | Memory: 53.9 MB (20.79%) - LeetSync

--- 55-jump-game/jump-game.js
+++ 55-jump-game/jump-game.js
@@ -5,12 +5,9 @@
  */
 var canJump = function(nums) {
     let reach  = 0;
-    console.log(nums.length)
     for (let i = 0; i <= reach; i++) {
       reach = Math.max(reach, i+ nums[i])
-      console.log(reach, i)
       if (reach >= nums.length - 1) {
-          console.log(reach, i , nums[i])
          return true;
       }
     }

8285371b65622c0f79c2061b20290d617e30be3e
Nam Le <39086067+namhihi237@users.noreply.github.com>
2024-08-25 22:53:40 +0700

    Time: 93 ms (77.72%) | Memory: 57.8 MB (52.07%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.js
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.js
@@ -4,29 +4,30 @@
  * @return {number}
  */
 var countCharacters = function (words, chars) {
-  const charCount = {};
+  let sum = 0;
+  const charsMap = new Map();
   for (const char of chars) {
-    charCount[char] = (charCount[char] || 0) + 1;
+    charsMap.set(char, charsMap.get(char) ? charsMap.get(char) + 1 : 1);
   }
 
-  let sum = 0;
   for (const word of words) {
-    if (canFormWord(word, charCount)) {
+    let ok = true;
+    let charsMapClone = new Map([...charsMap]);
+
+    for (let i = 0, len = word.length; i < len; ++i) {
+      const char = word[i];
+      if (charsMapClone.get(char)) {
+        charsMapClone.set(char, charsMapClone.get(char) - 1);
+      } else {
+        ok = false;
+        break;
+      }
+    }
+
+    if (ok) {
       sum += word.length;
     }
-  }
+  };
 
   return sum;
-};
-
-function canFormWord(word, charCount) {
-  const count = {};
-  for (let i = 0; i < word.length; ++i) {
-    const char = word[i];
-    if (!charCount[char] || count[char] >= charCount[char]) {
-      return false;
-    }
-    count[char] = (count[char] || 0) + 1;
-  }
-  return true;
 }
